0194dcfeb761260b363c87f57ddfc2fb25ba822b
felipe <bac.smart.idea@gmail.com>
2024-06-25 18:57:52 +0000

    ajustes de frontend

--- pages/Semoventes/jsobjects/ImagemLocal/ImagemLocal.js
+++ pages/Semoventes/jsobjects/ImagemLocal/ImagemLocal.js
@@ -20,8 +20,15 @@ export default {
 	},
 	ImagemTabelaEsquerda () {
 		ImagemLocal.esquerda = LinkUrl.url_App_Smith + semoventes_ativos.triggeredRow.Foto_esquerda.formats.thumbnail.url
+	},
+	
+	ImagemTabelaFrenteDesativado () {
+		ImagemLocal.frente = LinkUrl.url_App_Smith + SemoventesDesativados.triggeredRow.Foto_frente.formats.thumbnail.url
+	},
+	ImagemTabelaDireitaDesativado () {
+		ImagemLocal.direita = LinkUrl.url_App_Smith + SemoventesDesativados.triggeredRow.Foto_direita.formats.thumbnail.url
+	},
+	ImagemTabelaEsquerdaDesativado () {
+		ImagemLocal.esquerda = LinkUrl.url_App_Smith + SemoventesDesativados.triggeredRow.Foto_esquerda.formats.thumbnail.url
 	}
-
 }
-
-frente: ""
