1af6cbc7d6ec10b5b38252ac4e310ad8a3174869
Bac Smart Idea <bac.smart.idea@gmail.com>
2024-09-12 19:55:56 +0000

    criação das queries e função de rodar gets para prontuario

---
+++ pages/Semoventes/jsobjects/rodarGets/rodarGets.js
@@ -0,0 +1,15 @@
+export default {
+	getsProntuario () {
+		getBanho.run();
+		getColeira.run();
+		getConsulta.run();
+		getEcto.run();
+		getLiberdade.run();
+		getMedicamento.run();
+		getPeso.run();
+		getRacao.run();
+		getRasqueamento.run();
+		getVacina.run();
+		getVerm.run();
+	}
+}
